c2e3c9a1cee121ad43aebd30f7459d7d84492d78
Albert Wu <38256449+CrazyAncestor@users.noreply.github.com>
2024-07-27 19:44:29 -0500

    Add files via upload

--- TMM_Tamm.m
+++ TMM_Tamm.m
@@ -3,7 +3,7 @@
 lambda0 = 300;  %   unit: um
 
 %       Refraction index
-n_Au = 5.4-1.75i;
+n_Au = 400-500i;
 n_Si = 3.4;
 n_Vac = 1.;
 
@@ -15,6 +15,10 @@ d_Vac = 0.25*lambda0/n_Vac;
 %       DBR mirror multilayer total number
 DBR_layer = 5;
 
+%   Build DBR & Tamm cavity parameters
+DBR = make_DBR(d_Si,n_Si,d_Vac,n_Vac,DBR_layer);
+Tamm = make_Tamm(DBR, d_Au, n_Au);
+
 %   Wavenumbers
 k = 2*pi*linspace(0.6/(lambda0),1.4/(lambda0),1001);
 
@@ -24,14 +28,15 @@ reflections_Tamm = [];
 reflections_DBR = [];
 
 for i=1:length(k)
-reflections_Tamm = [reflections_Tamm,Tamm_reflection(k(i),d_Si,n_Si,d_Vac,n_Vac,d_Au,n_Au,DBR_layer)];
-reflections_DBR = [reflections_DBR,DBR_reflection(k(i),d_Si,n_Si,d_Vac,n_Vac,DBR_layer)];
+    [r_DBR, field_DBR] = TMM_analysis(k(i),DBR,0);
+    [r_Tamm, field_Tamm] = TMM_analysis(k(i),Tamm,0);
+    reflections_DBR = [reflections_DBR , r_DBR];
+    reflections_Tamm = [reflections_Tamm , r_Tamm];
 end
 
 %   Calculate Tamm mode field distribution
-d = [d_Si,d_Vac,d_Si,d_Vac,d_Si,d_Au];
-n = [n_Si,n_Vac,n_Si,n_Vac,n_Si,n_Au];
-field = multilayer_field_distribution(k(500),d,n,100);
+[min_value, k_idx] = min(reflections_Tamm);
+[r_Tamm, field_Tamm] = TMM_analysis(k(k_idx),Tamm,100);
 
 %   Plot reflection coefficients vs. frequency
 figure
@@ -50,14 +55,26 @@ hold off
 %   Plot Tamm mode field frequency
 figure
 hold on
-x = field(1,:);
-E = field(2,:);
+x = field_Tamm(1,:);
+E = field_Tamm(2,:);
+block_x = [0];
+x_present = 0;
+Height = max(E);
+for i=1:DBR_layer
+    if mod(i,2)==1
+        block_x=[block_x,d_Si+x_present]; 
+        x_present = x_present + d_Si;
+        patch('Faces',[1 2 3 4],'Vertices',[ block_x(i) 0; block_x(i+1) 0; block_x(i+1) Height; block_x(i) Height],'FaceColor',[198 198 198]./255)
+    else
+        block_x=[block_x,d_Vac+x_present];
+        x_present = x_present + d_Vac;
+        patch('Faces',[1 2 3 4],'Vertices',[ block_x(i) 0; block_x(i+1) 0; block_x(i+1) Height; block_x(i) Height],'FaceColor',[255 255 255]./255)
+    end
+    
+end
 plot(x,E)
-
-%legend('Tamm reflection','DBR reflection')
-
-%xlabel('Frequency(THz)')
-%ylabel('Reflection')
+xlabel('position(um)')
+ylabel('Electric Field Enhancement')
 
 hold off
 
@@ -65,64 +82,49 @@ hold off
 %   Transfer matrix formula (Macleod, H. A. (Hugh A. (2001). Thin-film optical filters / H.A. Macleod. (Third edition.). Institute of Physics Pub.)
 function M = transfer_matrix(k,d,n)
     delta = k*n*d;
-    M = [cos(delta),sin(delta)*n*1i;sin(delta)/n*1i,cos(delta)];
-end
-
-%   Reflection coefficient for multilayer structure
-function r = multilayer_reflection(k,d,n)
-
-    %   Error message when giving different sizes of thicknesses d and
-    %   refractive index n.
-    N = length(d);
-    if length(d)~=length(n)
-        prompt={'Mismatch size of n and d'};
-        return
-    end
-    
-    %   Calculate transfer matrix M, iterating for different layers.
-    E_H_exit = [1;1]; % E and H fields at the end of the multilayer
-    M = [1,0;0,1];
-    for m=1:N
-        M = M * transfer_matrix(k,d(m),n(m));
-    end
-    
-    %   Calculate field amplitude reflection coefficient
-    E_H_in = M * E_H_exit;
-    C_B = E_H_in(2) / E_H_in(1);
-    r = (1-C_B) / (1+C_B);
+    M = [cos(delta),sin(delta)/n*1i;sin(delta)*n*1i,cos(delta)];
 end
 
 %   Electric field amplitude spatial distribution for multilayer structure
-function field = multilayer_field_distribution(k,d,n,N_mesh)
-
-    %   Error message when giving different sizes of thicknesses d and
-    %   refractive index n.
-    N = length(d);
-    if length(d)~=length(n)
-        prompt={'Mismatch size of n and d'};
-        return
-    end
+function [r, field] = TMM_analysis(k,parameters,N_mesh)
     
-    %   Calculate transfer matrix M, iterating for different layers.
+    %   Giving thickness and refractive index
+    d = parameters(1,:);
+    n = parameters(2,:);
+    
+    %   Defining variables
     E_H_exit = [1;1]; % E and H fields at the end of the multilayer
-    x = [0];
-    x_present = 0;
+    x = [sum(d)];
+    x_present = sum(d);
     E = [1];
     E_H = E_H_exit;
-
+    
+    %   Calculating the E,H fields with transfer matrices
+    N = length(d);
     for m=1:N
-        for j=1:N_mesh
-            M = transfer_matrix(k,d(m)/N_mesh,n(m));
+        ms = N-m+1;
+        if N_mesh==0
+            M = transfer_matrix(k,d(ms),n(ms));
             E_H = M * E_H;
-            x_present = x_present + d(m)/N_mesh;
-            x = [x,x_present];
-            E = [E, abs(E_H(1))];
+        else
+            for j=1:N_mesh
+                M = transfer_matrix(k,d(ms)/N_mesh,n(ms));
+                E_H = M * E_H;
+                x_present = x_present - d(ms)/N_mesh;
+                x = [x, x_present];
+                E = [E, abs(E_H(1))];
+            end
         end
     end
-    field = [x;E];
+    field = [x;E/E(length(E))];
+    
+    %   Calculating reflection coefficients
+    n_eff = E_H(2) / E_H(1);
+    r = (1-n_eff) / (1+n_eff);
+    r = r * conj(r);
 end
 
-function R = DBR_reflection(k,d1,n1,d2,n2,N)
+function DBR = make_DBR(d1,n1,d2,n2,N)
     
     %   Define list of thickness d and refractive index n
     d = [];
@@ -139,33 +141,13 @@ function R = DBR_reflection(k,d1,n1,d2,n2,N)
             n = [n,n2];
         end
     end
-
-    %   Calculate power reflection of DBR mirror
-    r = multilayer_reflection(k,d,n);
-    R = r*conj(r);
+    DBR = [d;n];
 end
 
-function R = Tamm_reflection(k,d1,n1,d2,n2,d_metal,n_metal,N)
-    %   Define list of thickness d and refractive index n
-    d = [];
-    n = [];
-
-    %   DBR parameter
-    for m=1:N
-        if mod(m,2)==1
-            d = [d,d1];
-            n = [n,n1];
-        else
-            d = [d,d2];
-            n = [n,n2];
-        end
-    end
-
-    %   Metal layer parameter
+function Tamm = make_Tamm(DBR,d_metal,n_metal)
+    d = DBR(1,:);
+    n = DBR(2,:);
     d = [d,d_metal];
     n = [n,n_metal];
-    
-    %   Power reflection
-    r = multilayer_reflection(k,d,n);
-    R = r*conj(r);
+    Tamm = [d;n];
 end
